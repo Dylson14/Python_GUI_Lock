e231d50571dca1e4ad66ebfb2b31b6358fbd06cc
Dylson14 <dylsonuni@gmail.com>
2025-12-13 22:20:51 +0000

    add the c++ code to control the servo motor

--- Locking Mechanism/src/main.cpp
+++ Locking Mechanism/src/main.cpp
@@ -1,15 +1,17 @@
 #include <Arduino.h> 
+#include <Servo.h>
 
-// the setup function runs once when you press reset or power the board
+Servo myServo;
+
+int angle = 90;
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+myServo.attach(9);
+
+Serial.begin(9600);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(100);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(100);                      // wait for a second
+myServo.write(angle);
+delay(15);
 }
