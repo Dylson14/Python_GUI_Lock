c879ee7e77ace2dbd36380fd31c2108e7f4e775f
Dylson14 <dylsonuni@gmail.com>
2025-12-14 18:05:28 +0000

    project completed

--- Locking Mechanism/src/main.cpp
+++ Locking Mechanism/src/main.cpp
@@ -1,39 +1,45 @@
-#include <Arduino.h> 
+#include <Arduino.h>
 #include <Servo.h>
 
 Servo myServo;
 
 int angle = 120;
 
-void setup() {
-myServo.attach(9);
+void setup()
+{
+  myServo.attach(9);
 
-Serial.begin(9600);
+  myServo.write(100);
+
+  Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
 
   // Check if serial data is available
-  if (Serial.available() > 0) {
+  if (Serial.available() > 0)
+  {
     // Read the incoming string until newline character
     String command = Serial.readStringUntil('\n');
-    
+
     // Remove any whitespace/carriage returns
     command.trim();
     command.toLowerCase();
-    
+
     // Compare command and set angle
-    if (command == "locked") {
-      angle = 180;
+    if (command == "locked")
+    {
+      angle = 178;
       Serial.println("Moving to locked position (180 degrees)");
     }
-    else if (command == "unlocked") {
-      angle = 30;
+    else if (command == "unlocked")
+    {
+      angle = 10;
       Serial.println("Moving to unlocked position (30 degrees)");
     }
   }
 
-
-myServo.write(angle);
-delay(15);
+  myServo.write(angle);
+  delay(15);
 }
